4a3170f9c728a209631240cb11d526f1aa976e85
Harish Mahato <101078273+harishm-99@users.noreply.github.com>
2023-11-28 18:50:45 +0000

    operator assignment started

---
+++ chapters/chapters.js
@@ -0,0 +1,9 @@
+let a=10;
+
+console.log("++a", ++a);//11
+console.log("a++", a++);//11
+console.log("--a", --a);//11
+console.log("a--", a--);//11
+console.log("a = ", a);//10
+
+console.log("a-- = ", a--);//10
---
+++ practice/Operator_Assignment.js
@@ -0,0 +1,53 @@
+// Problem 1: Write a JavaScript program to perform arithmetic operations on two numbers (addition, subtraction, multiplication, division).
+
+let n1 = 5;
+let n2 = 10;
+
+console.log("The Arithmetic Operations between the numbers are :")
+
+function addition() {
+    let sum = n1 + n2;
+    return sum;
+}
+
+function subtraction() {
+    let sub = n1 + n2;
+    return sub;
+}
+
+function multiplication() {
+    let mul = n1 * n2;
+    return sum;
+}
+
+function addition() {
+    let sum = n1 + n2;
+    return sum;
+}
+
+
+// Problem 2: Write a JavaScript program to demonstrate the use of assignment operators on variables.
+
+
+// Problem 3: Write a JavaScript program to compare two numbers and log whether they are equal, greater, or lesser.
+
+
+// Problem 4: Write a JavaScript program to demonstrate logical operators in a conditional statement.
+
+
+// Problem 5: Write a JavaScript program to perform bitwise operations on two numbers.
+
+
+// Problem 6: Write a JavaScript program to use the ternary operator to determine the larger of two numbers.
+
+
+// Problem 7: Write a JavaScript program to demonstrate type operators on different data types.
+
+
+// Problem 8: Write a JavaScript program to use string operators to concatenate two strings.
+
+
+// Problem 9: Write a JavaScript program to demonstrate the use of the typeof operator on different variables.
+
+
+// Problem 10: Write a JavaScript program to use the in operator to check if a property exists in an object.
