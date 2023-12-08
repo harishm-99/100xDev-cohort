1975b5bdcdbb67893677ce96880530d4a0218d87
Harish Mahato <101078273+harishm-99@users.noreply.github.com>
2023-12-08 17:47:58 +0000

    done

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -5,40 +5,6 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
-
-// transactions = [
-//   { itemName: "MAC Pro", category: "Electronics", price: 2000, timestamp: "10:00 PM" },
-//   { itemName: "Chair", category: "Furniture", price: 30, timestamp: "11:20 PM" },
-//   { itemName: "Surf Excel Soap", category: "Soaps", price: 10, timestamp: "11:10 PM" }
-// ];
-
-// function calculateTotalSpentByCategory(transactions) {
-//   let result = {};
-  
-//   for (let i = 0; i < transactions.length; i++) {
-//     let transaction = transactions[i];
-//     let category = transaction.category;
-//     let totalAmountSpent = transaction.price;
-    
-//     // If the category doesn't exist in the result object, create it
-//     if (!result[category]) {
-//       result[category] = 0;
-//     }
-    
-//     // Add the total amount spent to the existing category
-//     result[category] += totalAmountSpent;
-//   }
-  
-//   // Convert the result object into an array of objects with the specified formatxxx
-//   let resultArray = Object.keys(result).map(category => ({ [category]: result[category] }));
-  
-//   return resultArray;
-// }
-
-// console.log(calculateTotalSpentByCategory(transactions));
-
-// module.exports = calculateTotalSpentByCategory;
-
 function calculateTotalSpentByCategory(transactions) {
 	let output = [];
 
