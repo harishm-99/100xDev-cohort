fcb8db4bc99c2b0f5cd62cb6460fcdbd3c2a540e
Harish Mahato <101078273+harishm-99@users.noreply.github.com>
2023-12-08 17:41:24 +0000

    expenditure & Analysis

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,40 +6,71 @@
 */
 
 
+// transactions = [
+//   { itemName: "MAC Pro", category: "Electronics", price: 2000, timestamp: "10:00 PM" },
+//   { itemName: "Chair", category: "Furniture", price: 30, timestamp: "11:20 PM" },
+//   { itemName: "Surf Excel Soap", category: "Soaps", price: 10, timestamp: "11:10 PM" }
+// ];
 
+// function calculateTotalSpentByCategory(transactions) {
+//   let result = {};
+  
+//   for (let i = 0; i < transactions.length; i++) {
+//     let transaction = transactions[i];
+//     let category = transaction.category;
+//     let totalAmountSpent = transaction.price;
+    
+//     // If the category doesn't exist in the result object, create it
+//     if (!result[category]) {
+//       result[category] = 0;
+//     }
+    
+//     // Add the total amount spent to the existing category
+//     result[category] += totalAmountSpent;
+//   }
+  
+//   // Convert the result object into an array of objects with the specified formatxxx
+//   let resultArray = Object.keys(result).map(category => ({ [category]: result[category] }));
+  
+//   return resultArray;
+// }
 
+// console.log(calculateTotalSpentByCategory(transactions));
 
-transactions = [
-  { itemName: "MAC Pro", category: "category1", price: 2000, timestamp: "10:00 PM" },
-  { itemName: "Xbox 1050", category: "category1", price: 1000, timestamp: "11:00 PM" },
-  { itemName: "Chair", category: "category2", price: 30, timestamp: "11:20 PM" },
-  { itemName: "Surf Excel Soap", category: "category3", price: 10, timestamp: "11:10 PM" }
-];
+// module.exports = calculateTotalSpentByCategory;
 
 function calculateTotalSpentByCategory(transactions) {
-  let result = {};
-  
-  for (let i = 0; i < transactions.length; i++) {
-    let transaction = transactions[i];
-    let category = transaction.category;
-    let totalAmountSpent = transaction.price;
-    
-    // If the category doesn't exist in the result object, create it
-    if (!result[category]) {
-      result[category] = 0;
-    }
-    
-    // Add the total amount spent to the existing category
-    result[category] += totalAmountSpent;
-  }
-  
-  // Convert the result object into an array of objects with the specified formatxxx
-  let resultArray = Object.keys(result).map(category => ({ [category]: result[category] }));
-  
-  return resultArray;
+	let output = [];
+
+	// if there are no transactions return an empty array
+	if (transactions.length === 0) {
+		return output;
+	}
+
+	// first store all the categories along with their respective total price
+	let categoryWithAmount = {};
+
+	for (let transaction of transactions) {
+		const category = transaction.category;
+
+		if (categoryWithAmount[category]) {
+			categoryWithAmount[category] += transaction.price;
+		} else {
+			categoryWithAmount[category] = transaction.price;
+		}
+	}
+
+	// now iterate over all the categories, make and object which will contain the category name and total amount and then add it to the output array
+	for (let category in categoryWithAmount) {
+		output.push({
+			category: category,
+			totalSpent: categoryWithAmount[category],
+		});
+	}
+
+	console.log(output);
 }
 
-console.log(calculateTotalSpentByCategory(transactions));
-
+calculateTotalSpentByCategory({ itemName:"Tea Powder", category:"Tea", price:25, timestamp:"12:00 PM" });
 module.exports = calculateTotalSpentByCategory;
 
