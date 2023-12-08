64d8e1464bc4cc0fccb4a2198f22fe12345a5840
Harish Mahato <101078273+harishm-99@users.noreply.github.com>
2023-12-08 12:07:19 +0000

    angagram test passed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase().split("").sort();
+  str2 = str2.toLowerCase().split("").sort();
 
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < str1.length; i++){
+    if (str1[i] !== str2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
+console.log(isAnagram("harish","rahish"));
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -5,8 +5,51 @@
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
 
+let category1 = [{
+  itemName: "MAC Pro",
+  category: "category1",
+  price: 2000,
+  timestamp: "10:00 PM"
+},
+{
+  itemName: "Xbox 1050",
+  category: "category1",
+  price: 1000,
+  timestamp: "11:00 PM"
+}
+];
+
+let total_amount_spent_on_category1 = 0;
+for (let i = 0; i < category1.length; i++) {
+  total_amount_spent_on_category1 = total_amount_spent_on_category1 + category1[i].price;
+}
+console.log(total_amount_spent_on_category1);
+
+let category2 = [{
+  itemName: "Chair",
+  category: "Home",
+  price: 30,
+  timestamp: "11:20 PM"
+},
+{
+  itemName: "Surf Excel Soap",
+  category: "Home",
+  price: 10,
+  timestamp: "11:10 PM"
+}
+];
+
+let total_amount_spent_on_category2 = 0;
+for (let i = 0; i < category2.length; i++) {
+  total_amount_spent_on_category2 = total_amount_spent_on_category2 + category2[i].price;
+}
+console.log(total_amount_spent_on_category2);
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  transactions = [{ category1, total_amount_spent_on_category1 }, { category2, total_amount_spent_on_category2 }];
+  return [transactions];
 }
 
-module.exports = calculateTotalSpentByCategory;
+console.log(calculateTotalSpentByCategory(transactions)); 
+
+// module.exports = calculateTotalSpentByCategory;
