60543b979973f5f0208d4d8107161c2100a8bef7
Harish Mahato <101078273+harishm-99@users.noreply.github.com>
2023-12-14 13:29:50 +0000

    classes

--- assignments/week-1/offline-class-1/level-1/arrayPractice.js
+++ assignments/week-1/offline-class-1/level-1/arrayPractice.js
@@ -16,3 +16,22 @@ console.log(arr);
 //unshift - adds one at the beginning
 let op4 = arr.unshift(98);
 console.log(arr);
+
+//concat - adding two arrays
+let arr2 = [4, 5, 6];
+console.log(arr.concat(arr2));
+// Anotherway
+let arrFinal = [];
+arrFinal = arrFinal.concat(arr,arr2);
+console.log("Array Final : "+arrFinal);
+
+//foreach() - For each element in an array
+
+// console.log(arr);
+function print(str) {
+    console.log(str);
+}
+
+arr.forEach(print);
+
+//Later - maps,filter,find,sort
---
+++ assignments/week-1/offline-class-1/level-2/classPractice.js
@@ -0,0 +1,47 @@
+// Class, Math, JSON, Date, Object
+
+const dog = {
+    name: "Dog",
+    speaks: "bhow bhow",
+    legCount:"4"
+}
+const cat = {
+    name: "Cat",
+    speaks: "meow meow",
+    legCount:"4"
+}
+
+function animalIs(animal) {
+    console.log("The animal is " + animal['name'] +" and it speaks "+ animal['speaks']);
+}
+
+// console.log(`The animal is ${dog.name} and it speaks ${dog.speaks}`);
+// console.log("The animal is " + dog['name'] +" and it speaks "+ dog['speaks']);
+// console.log("The animal is " + dog.name +" and it speaks "+ dog.speaks);
+
+// console.log("The animal is " + cat['name'] +" and it speaks "+ cat['speaks']);
+
+animalIs(dog);
+animalIs(cat);
+
+//The same results can be obtained through classes
+
+class Animal{
+    constructor(name, legCount, speaks){
+        this.name = name;
+        this.legCount = legCount;
+        this.speaks = speaks;
+    }
+}
+
+//Don't do this
+// let dog1 = {
+//     name: 'Dog',
+//     legsCount: '4',
+//     speaks: 'bhow bhow'
+// }
+
+//Do This
+let dog1 = new Animal('Dog', '4','bhow bhow');
+
+console.log('The animal is' , dog1.name, ".It speaks "+dog1.speaks+" and has ", dog1.legCount," legs")
