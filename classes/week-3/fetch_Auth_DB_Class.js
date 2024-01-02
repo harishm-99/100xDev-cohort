8b946c431c744086e203f3a17ec10e8b4115987c
Harish Mahato <101078273+harishm-99@users.noreply.github.com>
2024-01-03 00:25:58 +0530

    Add files via upload

---
+++ classes/week-3/fetch_Auth_DB_Class.js
@@ -0,0 +1,143 @@
+// hasing -  simple string converted into hashes, It is a oneway. given the output noone can find input. changing the input a bit xchanges the output by a lot
+// Encryption - Simple string converted into protetcted shit which can be decrpted too. given output can get the input
+
+
+// Encryption is two way hasing is on wyway hashing
+// doesn`t  require password
+
+// 3. json webtokens - take some json input and give us some structured data.anyone can see the original output given the signature. bt can only be verified by the bank
+
+// const express = require("express");
+// const jwt = require("jsonwebtoken");
+// const jwtPassword = "123456";
+
+// const app = express();
+
+// const ALL_USERS = [
+//   {
+//     username: "harkirat@gmail.com",
+//     password: "123",
+//     name: "harkirat singh",
+//   },
+//   {
+//     username: "raman@gmail.com",
+//     password: "123321",
+//     name: "Raman singh",
+//   },
+//   {
+//     username: "priya@gmail.com",
+//     password: "123321",
+//     name: "Priya kumari",
+//   },
+// ];
+
+// function userExists(username, password) {
+//   // write logic to return true or false if this user exists
+//   // in ALL_USERS array
+// }
+
+// app.post("/signin", function (req, res) {
+//   const username = req.body.username;
+//   const password = req.body.password;
+
+//   if (!userExists(username, password)) {
+//     return res.status(403).json({
+//       msg: "User doesnt exist in our in memory db",
+//     });
+//   }
+
+//   var token = jwt.sign({ username: username }, "shhhhh");
+//   return res.json({
+//     token,
+//   });
+// });
+
+// app.get("/users", function (req, res) {
+//   const token = req.headers.authorization;
+//   try {
+//     const decoded = jwt.verify(token, jwtPassword);
+//     const username = decoded.username;
+//     // return a list of users other than this username
+//   } catch (err) {
+//     return res.status(403).json({
+//       msg: "Invalid token",
+//     });
+//   }
+// });
+
+// app.listen(3000)
+
+
+// jwt is a common standard to encrypt data
+
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const jwtPassword = "123456";
+
+const app = express();
+app.use(express.json());
+
+const ALL_USERS = [
+  {
+    username: "harkirat@gmail.com",
+    password: "123",
+    name: "harkirat singh",
+  },
+  {
+    username: "raman@gmail.com",
+    password: "123321",
+    name: "Raman singh",
+  },
+  {
+    username: "priya@gmail.com",
+    password: "123321",
+    name: "Priya kumari",
+  },
+];
+
+function userExists(username, password) {
+  // write logic to return true or false if this user exists
+  // in ALL_USERS array
+  // hard todo - try to use the find function in js
+  let userExists = false;
+  for (let i = 0; i<ALL_USERS.length; i++) {
+    if (ALL_USERS[i].username == username && ALL_USERS[i].password == password) {
+      userExists = true;
+    }
+  }
+  return userExists;
+}
+
+app.post("/signin", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!userExists(username, password)) {
+    return res.status(403).json({
+      msg: "User doesnt exist in our in memory db",
+    });
+  }
+
+  var token = jwt.sign({ username: username }, jwtPassword);
+  return res.json({
+    token,
+  });
+});
+
+app.get("/users", function (req, res) {
+  const token = req.headers.authorization;
+  const decoded = jwt.verify(token, jwtPassword);
+  const username = decoded.username;
+  // return a list of users other than this username
+  res.json({
+    users: ALL_USERS.filter(function(value) {
+      if (value.username == username) {
+        return false
+      } else {
+        return true;
+      }
+    })
+  })
+});
+
+app.listen(3000)
