dd964ccfe2c565fffa84f8e10f643060a8832585
Harish Mahato <harishkr999999@gmail.com>
2024-03-31 15:21:04 +0530

    be completed

--- classes/week-5/todo-app/backend/index.js
+++ classes/week-5/todo-app/backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { createTodo, updateTodo } = require('./types');
+const { todo } = require('./db');
 const app = express();
 
 app.use(express.json());
 
-app.post('/todos', function (req, res) {
+app.post('/todos',async function (req, res) {
     const createPayLoad = req.body;
     const parsePayLoad = createTodo.safeParse(createPayLoad);
 
@@ -14,13 +15,24 @@ app.post('/todos', function (req, res) {
         });
         return;
     }
+
+    await todo.create({
+        title: createPayLoad.title,
+        description: createPayLoad.description
+    })
+    res.json({
+        msg:'Todo Created'
+    })
 })
 
-app.get('/todos', function (req, res) {
-    
+app.get('/todos',async function (req, res) {
+    const todos = await todo.find({});
+    res.json({
+        todos
+    })
 })
 
-app.put('/completed', function (req, res) {
+app.put('/completed',async function (req, res) {
     const updatePayLoad = req.body;
     const parsePayLoad = updateTodo.safeParse(updatePayLoad);
 
@@ -30,4 +42,12 @@ app.put('/completed', function (req, res) {
         });
         return
     }
+    await todo.update({
+        _id: updatePayLoad.id
+    }, {
+        completed:true
+    })
+    res.json({
+        msg:'Todo marked as completed'
+    })
 })
