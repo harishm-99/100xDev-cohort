0995729dfdf28e33d951fb2c6906bfe5836dcf02
Harish Mahato <harishkr999999@gmail.com>
2024-03-31 12:11:36 +0530

    index.js updated

--- classes/week-5/todo-app/backend/index.js
+++ classes/week-5/todo-app/backend/index.js
@@ -1,16 +1,33 @@
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express();
 
 app.use(express.json());
 
 app.post('/todos', function (req, res) {
-    
+    const createPayLoad = req.body;
+    const parsePayLoad = createTodo.safeParse(createPayLoad);
+
+    if (!parsePayLoad.success) {
+        res.status(411).json({
+            msg: 'You sent the wrong inputs',
+        });
+        return;
+    }
 })
 
 app.get('/todos', function (req, res) {
     
 })
 
-app.post('/completed', function (req, res) {
-    
+app.put('/completed', function (req, res) {
+    const updatePayLoad = req.body;
+    const parsePayLoad = updateTodo.safeParse(updatePayLoad);
+
+    if (!parsePayLoad.success) {
+        res.status(411).json({
+            msg: 'You sent the wrong inputs'
+        });
+        return
+    }
 })
