c17d95841f893d12fba2d4dd157bdaf49363945b
Harish Mahato <harishkr999999@gmail.com>
2024-03-31 11:56:49 +0530

    zod-types added

---
+++ classes/week-5/todo-app/backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/todos', function (req, res) {
+    
+})
+
+app.get('/todos', function (req, res) {
+    
+})
+
+app.post('/completed', function (req, res) {
+    
+})
