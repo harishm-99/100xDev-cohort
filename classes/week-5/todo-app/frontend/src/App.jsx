b80451d38ce95be4b90674ed8943d645ce834f70
Harish Mahato <harishkr999999@gmail.com>
2024-03-31 15:57:26 +0530

    created fe

---
+++ classes/week-5/todo-app/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { CreateTodo } from './components/CreateTodo';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <CreateTodo></CreateTodo>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ classes/week-5/todo-app/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,11 @@
+export function CreateTodo() {
+    
+    return (
+        <div>
+            <input type="text" placeholder="title" />
+            <input type="text" placeholder="description" />
+
+            <button>Add a Todo</button>
+        </div>
+    )
+}
