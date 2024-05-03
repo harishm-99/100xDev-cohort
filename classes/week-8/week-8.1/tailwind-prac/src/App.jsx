6ddbc413bca7801aedea670a548b4ec8ec3639fe
Harish Mahato <harishkr999999@gmail.com>
2024-05-03 07:39:04 +0530

    created tailwind-prac folder & moved files

---
+++ classes/week-8/week-8.1/tailwind-prac/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{display:'flex', justifyContent: "space-around"}}>
+        <div style ={{backgroundColor:"red"}}>Hi</div>
+        <div style ={{backgroundColor:"blue"}}>Hi</div>
+        <div style={{ backgroundColor: "green" }}>Hi</div>
+        <div style={{ backgroundColor: "orange" }}>Hi</div>
+        <div style={{ backgroundColor: "violet" }}>Hi</div>
+        </div>
+    </>
+  )
+}
+
+export default App
